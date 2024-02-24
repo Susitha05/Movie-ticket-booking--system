ea70c22790b4c5c25d67f51b38491faa195c3796
Isiwara Jayaratna <isiwarajayarathna2002@gmail.com>
2024-02-24 20:15:01 +0530

    Seat Selection - Doubly Linked List

--- Movie_ticket.java
+++ Movie_ticket.java
@@ -1,11 +1,48 @@
 import java.util.Scanner;
 import java.io.*;
 
+
+// Creating Seat class
+class Seat{   
+    int seatNumber;
+    boolean isBooked;
+    Seat prev;
+    Seat next;
+
+    Seat (int seatNumber){
+        this.seatNumber=seatNumber;
+        isBooked=false;
+        prev = Null;
+        next= Null;
+    }
+} 
+
 public class Movie_ticket {
     Scanner sc = new Scanner(System.in);
     String Username;
     int Password;
     public int num = 0;
+    Seat head;
+    Seat tail;
+// creating Linked list for seats
+    public Movie_ticket(){
+        for(int i=o;i<=50;i++){
+            addSeat(i);
+        }
+
+    }
+    public void addSeat (int seatNumber){
+        Seat nSeat = new Seat(seatNumber);
+        if(head==null){
+            head =nSeat;
+            tail =nSeat;
+        }else{
+            tail.next= nSeat;
+            nSeat.prev=tail;
+            tail=nSeat;
+        }
+
+    }
 
     public void login() {
         // check login
@@ -37,7 +74,7 @@ public class Movie_ticket {
 
         } else {
             System.out.println("Hi " + Username);
-            //HHI
+            
 
         }
 
@@ -52,7 +89,30 @@ public class Movie_ticket {
     }
 
     public void booking_man() {
+            System.out.println();
 
+            //Show available Seat Count
+            System.out.println("Available Seat Count: ");
+            Seat current = head;
+            while (current != Null) {
+                if (current.seatNumber == bookSeat) {
+                    if (current.isBooked==false) {
+                        current.isBooked=true;
+                        System.out.println("Seat Count" +bookSeat+ " booked Succesfully. ");
+                    
+
+                    }else{
+                        System.out.println("Seat Count" +bookSeat+ "Already Booked");
+                    }
+                    break;
+                   
+                }
+                current=current.next;
+
+            }   
+            if(current==Null){
+                System.out.println("invalid Seat Count");
+            }
     }
 
 }
